f80e1ff7f48a2e85bac04a61608f5ac3f1d7ea7a
MendozaJulioC <43477339+MendozaJulioC@users.noreply.github.com>
2021-08-18 14:21:18 -0500

    nueva version

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
-
-
 require('dotenv').config();
-process.env.NTBA_FIX_319 = 1;
+const express=require('express');
+const app = express();
+const port= process.env.PORT;
 const TelegramBot = require('node-telegram-bot-api');
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.token
-// Create a bot that uses 'polling' to fetch new updates
+process.env.NTBA_FIX_319 = 1;
+
 const bot = new TelegramBot(token, {polling: true});
 bot.on("polling_error", console.log);
+
 /*
 bot.on('message', (message) => {
   const chatId = message.chat.id;
-
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
 });
@@ -21,28 +21,22 @@ const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
-  })
-
-
-
+})
 
 bot.onText(/\/start/, (msg) => {
-
-    bot.sendMessage(msg.chat.id, "Welcome", {
+    bot.sendMessage(msg.chat.id, "Bienvenidos a la Medellín Futuro", {
         "reply_markup":
         {
             "keyboard": 
             [
-                ["Avance", "Cumplimiento", "Proyeccion"],   
-                ["Lineas", "Ejecución", "Alertas"]
+                ["Avance 🚀 "   , "Cumplimiento ✅", "Proyeccion 🥇"],   
+                ["Lineas📊 ", "Ejecución 🛠️", "Alertas 🚨"]
                ,
             ]
         }
     });
-    
  });
  
-
 var request= require('request')
  bot.onText(/\/avance/, function(message, match){
   //  let plan = match[1];
@@ -140,6 +134,49 @@ bot.onText(/\/lineas/, function(message, match){
     })
 });
 
+bot.onText(/\/ek0<<7/, function(message, match){
+    //  let plan = match[1];
+    let chatId= message.chat.id; 
+   
+    request(`https://sse-pdm.herokuapp.com/pa/api/avancefinanciero`, function(error, response,body){
+        if(!error && response.statusCode==200){
+            bot.sendMessage(chatId, "<b>Finanzas Proyectos de Inversión</b>", {parse_mode:"HTML"})
+            .then(function(msg){
+                var res=JSON.parse(body);
+                
+                   let poai =((res.data[0].poai))
+                   let pptoajustado=(((res.data[0].pptoajustado)))
+                   let pptoejecutado=(((res.data[0].pptoejecutado)))
+                   let compromisos=(((res.data[0].compromisos)))
+                   let disponible=(((res.data[0].disponible)))
+                   let ordenado=(((res.data[0].ordenado)))
+                   let porc_ejecutado= ((parseFloat(pptoejecutado)/parseFloat(pptoajustado))*100).toFixed(2)
+                   let total=(((res.data[0].total)))
+
+                   //let avancelinea= (res.data[index].avance_linea).substr(0,5)
+                   //ordenar.push([avancelinea])
+                  bot.sendMessage(chatId,   ' \nPOAI <strong>'+formatter.format(poai)+'</strong>'
+                                            +'\nPpto. Ajustado <strong> '+formatter.format(pptoajustado)+'</strong>'
+                                            +'\nPpto. Ejecutado <strong> '+formatter.format(pptoejecutado)+'</strong>'
+                                            +'\nCompromisos <strong> '+formatter.format(compromisos)+'</strong>'
+                                            +'\nDisponibles <strong> '+formatter.format(disponible)+'</strong>'
+                                            +'\nOrdenado <strong> '+formatter.format(ordenado)+'</strong>'
+                                            +'\n---------------------'
+                                            +'\nTotal <strong> '+formatter.format(total)+'</strong>'
+                                            +'\n%Ejecución <strong> '+porc_ejecutado+'%</strong>'
+                                            +'\n(cifras en millones de pesos)'
+                                         
+                                            ,{parse_mode:'HTML'})
+              
+               
+                
+            } )
+         }
+    })
+});
+
+
+
 bot.on('message', (msg) => {
     var avance = "Avance";
     if (msg.text.indexOf(avance) === 0) {
@@ -157,10 +194,8 @@ bot.on('message', (msg) => {
                           ordenar.push([res.data[index].avance])
                           cortes.push([fecha])
                           bot.sendMessage(chatId,  'Corte: '+cortes[index]+ ' \nAvance <strong> '+ordenar[index]+'% </strong>', {parse_mode:'HTML'}) 
-                        
                         }
                     }
-                    
                 } )
              }
            //  bot.sendMessage(chatId, 'Hola Mundo3!!!')
@@ -187,14 +222,12 @@ bot.on('message', (msg) => {
                         
                         }
                     }
-                    
                 } )
-             }
-           
+            }
         })
-
-
     }
+
+  
     var linea = "Lineas";
     if (msg.text.indexOf(linea) ===0 ) {
         bot.sendMessage(msg.chat.id, "<b style='color:red'>Resultados</b>", {parse_mode:"HTML"});
@@ -224,6 +257,7 @@ bot.on('message', (msg) => {
         })
 
     }
+
     var proyeccion = "Proyeccion";
     if (msg.text.indexOf(proyeccion) ===0 ) {
         bot.sendMessage(msg.chat.id, "<b style='color:red'>Resultados</b>", {parse_mode:"HTML"});
@@ -251,11 +285,7 @@ bot.on('message', (msg) => {
         })
 
     }
-
-
-
 });
-
 
 bot.onText(/\/ejecucionlinea (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
@@ -264,7 +294,7 @@ bot.onText(/\/ejecucionlinea (.+)/, (msg, match) => {
     request(`https://sse-pdm.herokuapp.com/pi/api/line/financiera/${resp}`, function(error, response,body){
         if(!error && response.statusCode==200){
            
-                bot.sendMessage(chatId, "<b>Finanzas Linea</b>", {parse_mode:"HTML"})
+                bot.sendMessage(chatId, "<b>Finanzas Línea: </b>"+resp, {parse_mode:"HTML"})
                 .then(function(msg){
                     var res=JSON.parse(body);
                     if(res.data[0].pptoajustado != null){
@@ -284,6 +314,12 @@ bot.onText(/\/ejecucionlinea (.+)/, (msg, match) => {
      })
 });
 
+
+
+// Listening
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
 
 
     
